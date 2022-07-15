346843100db42f7fbbc6f01b2fb360f6d1a4a3ab
shruti1405 <shrutiyadavhalkorwa@gmail.com>
2022-07-15 18:12:19 +0530

    Day8.cpp

---
+++ DS I/Day 8/Linked List Cycle.cpp
@@ -0,0 +1,15 @@
+class Solution
+{   public:
+    bool hasCycle(ListNode *head)
+    {
+        auto walker = head, runner = head;
+        while (runner && runner->next)
+        {
+            walker = walker->next;
+            runner = runner->next->next;
+            if (walker == runner)
+                return true;
+        }
+        return false;
+    }
+};
