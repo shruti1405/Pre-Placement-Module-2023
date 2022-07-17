31e7c3c6304de5355e4a7531a8df7dfcf76bd25b
shruti1405 <shrutiyadavhalkorwa@gmail.com>
2022-07-17 23:27:04 +0530

    Day 10.cpp

---
+++ DS I/Day 10/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{   public:
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        vector<int> nodes;
+        inorder(root, nodes);
+        return nodes;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& nodes) 
+    {
+        if (!root) 
+        {
+            return;
+        }
+        inorder(root -> left, nodes);
+        nodes.push_back(root -> val);
+        inorder(root -> right, nodes);
+    }
+};
