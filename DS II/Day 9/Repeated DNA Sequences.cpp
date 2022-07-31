409578b1c38aca9c99c218d1b6eac94dad6530ee
shruti1405 <shrutiyadavhalkorwa@gmail.com>
2022-07-31 23:44:28 +0530

    Day 9.cpp

---
+++ DS II/Day 9/Repeated DNA Sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   vector<string> findRepeatedDnaSequences(string s) {
+    char  hashMap[1048576] = {0};
+    vector<string> ans;
+    int len = s.size(),hashNum = 0;
+    if (len < 11) return ans;
+    for (int i = 0;i < 9;++i)
+        hashNum = hashNum << 2 | (s[i] - 'A' + 1) % 5;
+    for (int i = 9;i < len;++i)
+        if (hashMap[hashNum = (hashNum << 2 | (s[i] - 'A' + 1) % 5) & 0xfffff]++ == 1)
+            ans.push_back(s.substr(i-9,10));
+    return ans;
+}
+};
