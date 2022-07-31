4f56f0987ea6e0b27d8f3e20d07c6e3d1a6cb560
shruti1405 <shrutiyadavhalkorwa@gmail.com>
2022-07-31 23:47:05 +0530

    Day 9.cpp

--- DS II/Day 9/Repeated DNA Sequences.cpp
+++ DS II/Day 9/Repeated DNA Sequences.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-   vector<string> findRepeatedDnaSequences(string s) {
-    char  hashMap[1048576] = {0};
-    vector<string> ans;
-    int len = s.size(),hashNum = 0;
-    if (len < 11) return ans;
-    for (int i = 0;i < 9;++i)
-        hashNum = hashNum << 2 | (s[i] - 'A' + 1) % 5;
-    for (int i = 9;i < len;++i)
-        if (hashMap[hashNum = (hashNum << 2 | (s[i] - 'A' + 1) % 5) & 0xfffff]++ == 1)
-            ans.push_back(s.substr(i-9,10));
-    return ans;
+  vector<string> findRepeatedDnaSequences(string s) {
+    char flag[262144] ={0};
+    vector<string> result;
+    int len,DNA=0,i;
+    if((len=s.length())< 11) return result;
+    for(i = 0 ; i < 9; i++)
+        DNA = DNA << 2| (s[i] - 'A' + 1) % 5;
+    for(i = 9;i<len;i++)
+    {
+        DNA = (DNA<<2 & 0xFFFFF)|(s[i] - 'A' + 1)%5;
+            if(!(flag[DNA>>2]&(1<<((DNA&3) << 1)))) 
+                flag[DNA>>2] |= (1<<((DNA&3) << 1));
+            else if(!(flag[DNA>>2]&(2<<((DNA&3) << 1)))) 
+            {
+                result.push_back(s.substr(i-9,10));
+                flag[DNA>>2] |= (2<<((DNA&3) << 1));
+            }
+    }
+    return result;
 }
 };
